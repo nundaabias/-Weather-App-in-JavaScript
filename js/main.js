111a07d5cc4e69e8c9c11878d2b49b237bd13238
Nunda Abias <nundaabias@gmail.com>
2023-06-08 19:32:42 +0200

    weather api and icons added. js added

---
+++ js/main.js
@@ -0,0 +1,99 @@
+/*
+* Author: Abias Nunda
+* Year: 2023
+*/
+
+// GETTING THE MAIN SELECTORS
+const wrapper = document.querySelector(".weather-app"),
+  btnBack = document.querySelector("#btnBack"),
+  txtCity = document.querySelector("#txtCity"),
+  btnGet = document.querySelector("#btnGet"),
+  infoBox = document.querySelector("#infoBox"),
+  txtInfo = document.querySelector("#txtInfo"),
+  weatherIcon = document.querySelector('.weather-view img');
+
+const Key = "a4890b8df91d16a44c251e5c4955b816";
+let api = '';
+
+//BTN | TO GO BACK TO THE FORM VIEW
+btnBack.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+    txtCity.value = '';
+});
+
+// TXT BOX | ADDING SUBMIT FUCTION
+txtCity.addEventListener("keyup", (e) => {
+  if (e.key == "Enter" && txtCity.value != "") {
+    requestApi(txtCity.value);
+  }
+});
+
+// FUNCTION | USING THE API TO REQUEST WEATHER INFO
+const requestApi = (city) => {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Key}&units=metric`;
+  txtInfo.innerHTML = "Getting Info";
+  infoBox.classList.add("pending");
+  fetchData(api);
+};
+
+// BTN | GET THE USERS GEO INFO
+btnGet.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    // If the browser supports geolocation api
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    alert("Your browser does not support geolocation api");
+  }
+});
+const onSuccess = (position) => {
+    const {latitude, longitude} = position.coords; // getting lat and lon of the user divice 
+    api =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Key}&units=metric`;
+    fetchData();
+};
+
+const onError = (error) => {
+    txtInfo.innerHTML = error.message;
+    infoBox.classList.add("error");
+};
+
+// FUNCTION | GET API DATA
+const fetchData = () => {
+    fetch(api)
+    .then((response) => response.json())
+    .then((result) => waetherDetails(result));
+}
+
+// FUNCTION | CHECK IF CITY IS CORRECT AND DISPLAY INFO
+const waetherDetails = (info) => {
+    console.log(info);
+    if(info.cod == '404') {
+      txtInfo.innerHTML = `Cannot fint ${txtCity.value}`;
+      infoBox.classList.replace('pending', "error");
+    }else {
+        // GETTING THE VALUES FROM THE INFO OBJECT
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {feels_like,  humidity, temp} = info.main;
+
+        if(id == 800){
+            weatherIcon.src = './svg/day.svg';
+        }else if(id == 801 || id < 805){
+            weatherIcon.src = './svg/cloudy.svg';
+
+        }else if(id == 701 || id < 782){
+            weatherIcon.src = './svg/cloudy.svg';
+
+        }
+        // ADDING THE VALUES TO THE HTML
+        wrapper.querySelector(".temp .number").innerText = Math.floor(temp);
+        wrapper.querySelector(".weather-title").innerText = description;
+        wrapper.querySelector(".location .country").innerText = `${city}, ${country}`;
+        wrapper.querySelector(".app-footer .feels").innerText = Math.floor(feels_like);
+        wrapper.querySelector(".app-footer .humidity").innerText = Math.floor(humidity);
+
+      infoBox.classList.remove('pending', "error");
+      wrapper.classList.add("active");
+  
+    }
+  };
